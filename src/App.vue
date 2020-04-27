5791c4d1e4ef42d81a803beefeffe75fd2052301
DouglasPonce85 <douglasponce85@gmail.com>
2020-04-27 08:49:53 -0600

    Initializing project structure

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">Build-A-Bot</li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <RobotBuilder />
+    </main>
+  </div>
+</template>
+
+<script>
+// import HomePage from "./home/HomePage.vue";
+import RobotBuilder from "./build/RobotBuilder.vue";
+
+export default {
+  name: "App",
+  components: {
+    RobotBuilder
+  }
+};
+</script>
+
+<style>
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+</style>
