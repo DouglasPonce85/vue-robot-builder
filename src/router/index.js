70028fc9ce3d2679ca5e230e2a2925affb9190dc
DouglasPonce85 <douglasponce85@gmail.com>
2020-04-28 08:14:46 -0600

    Adding sidebar navigation

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,13 @@ import Router from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
-
 import BrowseParts from '../parts/BrowseParts.vue';
 import RobotHeads from '../parts/RobotHeads.vue';
 import RobotArms from '../parts/RobotArms.vue';
 import RobotTorsos from '../parts/RobotTorsos.vue';
 import RobotBases from '../parts/RobotBases.vue';
+import SidebarStandard from '../sidebars/SidebarStandard.vue';
+import SidebarBuild from '../sidebars/SidebarBuild.vue';
 
 Vue.use(Router);
 
@@ -18,11 +19,17 @@ export default new Router({
         {
             path: '/',
             name: 'Home',
-            component: HomePage
+            components: {
+                default: HomePage,
+                sidebar: SidebarStandard
+            }
         }, {
             path: '/build',
             name: 'Build',
-            component: RobotBuilder
+            components: {
+                default: RobotBuilder,
+                sidebar: SidebarBuild
+            }
         }, {
             path: '/parts/browse',
             name: 'BrowseParts',
